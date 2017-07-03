83e2fa715994250de2ead64f6d7b132e099a435a
mafevito <mafevito@hotmail.com>
2017-07-04 00:08:41 +0200

    Casi done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,64 @@
+/*jshint esversion:6 */
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
+    this.MINFLOOR   = 0;
+    this.waitingList = [];
+    this.passengers = [];
     this.requests   = [];
+    this.direction  = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    let init = setInterval(() => { update(); },1 * 1000); }
+
+  stop() {
+    clearInterval(init);
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {
+    if (waitingList === person.originFloor) {
+      this.passengers.push(person);
+      this.waitingList.splice();
+      this.requests.push();
+      console.log(`${person.name} has enter the elevator`);
+    }
+  }
+
+  _passengersLeave() {
+  }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+        console.log(`Posicion actual:${this.floor}`);
+        this.floor++;
+    }else{
+      console.log('Posicion máxima');
+    }
+  }
+
+  floorDown() {
+    if(this.floor > this.MINFLOOR) {
+      console.log(`Posicion actual:${this.floor}`);
+      this.floor--;
+    }else{
+      console.log('Posicion máxima');
+    }
+  }
+
+  call(person) {
+    this.requests.push(person);
+    this.waitingList.push(person.originFloor);
+   }
+
+  log() {
+    console.log(`Direction ${this.direction} | Position ${this.floor}`); }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,11 @@
+/*jshint esversion:6 */
+
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+let elevator = new Elevator();
+let marifer = new Person("Marifer", 0, 4);
+
+elevator.update();
+elevator.call(marifer);
